d031b76c7f5a9aed5611b54a1859289b8f06f8e0
Yu Yang <ulyssesyangyu@gmail.com>
2019-04-28 20:45:41 -0400

    add associations to agent and property model

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,4 @@
 class Agent < ApplicationRecord
+    # define relatinship with properties
+    has_many :properties, :class_name => "Property", :foreign_key => "agent_email"
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,5 @@
 class Property < ApplicationRecord
+    # define relatinship with agents
+    belongs_to :agent_1, class_name: "Agent", foreign_key: "agent_1_email"
+    belongs_to :agent_2, class_name: "Agent", foreign_key: "agent_2_email"
 end
